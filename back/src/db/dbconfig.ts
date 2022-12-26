8dd873d76b2d0d50bb789b98bfac83c21f11c2d9
Ronaldo Sc <rs.quim@gmail.com>
2022-12-26 12:09:51 -0300

    fix: removed comments in dbconfig.ts

--- back/src/db/dbconfig.ts
+++ back/src/db/dbconfig.ts
@@ -1,7 +1,7 @@
 import { DataSource } from "typeorm"
 import { InfractionSeverity, TimeTraveller, Violation } from "./entities"
-require("dotenv").config() // tem que ter o ()
-// o import ele retorna tipado como funçaõ, pode ser melhor que o require, teria que declarar num arquivo d.ts da tipagem de retorno
+require("dotenv").config()
+
 const db_URI = `postgres://postgres:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`
 
 export const dataORM = new DataSource({
@@ -11,6 +11,6 @@ export const dataORM = new DataSource({
 })
 
 export async function dbConfig() {
-  dataORM.setOptions({ url: db_URI }),
+  dataORM.setOptions({ url: db_URI })
   await dataORM.initialize()
 }
